05df3968bb87da04f0492c8529016b0e858ffbd1
Matteo Cajani <matteo.cajani@gmail.com>
2019-11-01 23:24:28 +0100

    simplify database and reviews apps

--- movie-reviews-service/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const appName = 'Movie reviews service';
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send(`Hello ${appName}!`));
-
-app.listen(port, () => console.log(`${appName} started`));
